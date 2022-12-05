e210a0ffe11258d0321a1835a3525a20272c3197
João Vitor Sobanski <54563534+joaosobanski@users.noreply.github.com>
2022-12-05 13:21:19 -0300

    tail

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col w-full text-center">
+      <span>teste</span>
+      <span>teste1</span>
+
+    </div>
+  );
+}
+
+export default App;
